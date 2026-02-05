55cbfefc2282ecb8128e0724ef3b224414ccdf19
umidovalimardon06 <umidovalimardon06@gmail.com>
2026-02-05 15:04:46 +0500

    progress

--- src/main/java/com/github/umidovalimardon06/Calculator.java
+++ src/main/java/com/github/umidovalimardon06/Calculator.java
@@ -1,4 +1,15 @@
 package com.github.umidovalimardon06;
 
-public class Calculator {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Calculator extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
--- src/main/java/com/github/umidovalimardon06/MainServlet.java
+++ src/main/java/com/github/umidovalimardon06/MainServlet.java
@@ -1,4 +1,24 @@
 package com.github.umidovalimardon06;
 
-public class MainServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getMethod());
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Hello from Servlet!");
+        printWriter.println("Its:"+ LocalDateTime.now());
+        String name = req.getParameter("name");
+        printWriter.println("Hello:"+name);
+    }
+
 }
